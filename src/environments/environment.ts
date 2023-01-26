27d0dbdb28ffe40f9b45f48b0ad5e696ddf1db6b
Nikorx16 <105009435+Nikorx16@users.noreply.github.com>
2023-01-26 10:54:32 +0100

    my wallet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC-fkih-RSY0huCHrw1wyjHFzcbR_Dg3r4",
-    authDomain: "virtual-wallet-46135.firebaseapp.com",
-    projectId: "virtual-wallet-46135",
-    storageBucket: "virtual-wallet-46135.appspot.com",
-    messagingSenderId: "165677638590",
-    appId: "1:165677638590:web:b0068fd3fff961c4780fef"
+    apiKey: "AIzaSyCd2Gm9lvUrVhZwpY2Oq7I8eTyUApXn08c",
+    authDomain: "my-wallet-a7c61.firebaseapp.com",
+    projectId: "my-wallet-a7c61",
+    storageBucket: "my-wallet-a7c61.appspot.com",
+    messagingSenderId: "669122922093",
+    appId: "1:669122922093:web:cbcc4db919b35fe285a164",
+    measurementId: "G-BGQ4LW74NC"
   }
 };
 /*
